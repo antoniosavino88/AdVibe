d59f683a6dc1de2f592cfb79442dedd5f36c8bd9
Barbara Perruci <barbara.perrucci1987@gmail.com>
2025-02-24 12:54:05 +0100

    installato fortify/require LiveWire-creazione compnents e mycss.css

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Vite -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>AdVibe</title>
+</head>
+<body>
+    <x-navbar/>
+    {{$slot}}
+</body>
+</html>
