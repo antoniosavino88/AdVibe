f809daea3427b8fd121d6b2310e9ce52be7659f3
Giacomo Spadafina <giacomospa69@gmail.com>
2025-02-26 17:35:04 +0100

    apportate modifiche

--- app/Models/Ad.php
+++ app/Models/Ad.php
@@ -9,8 +9,8 @@ use Laravel\Scout\Searchable;
 
 class Ad extends Model
 {
-    use Searchable;
-
+    // use Searchable;
+    
     protected $fillable = ['title', 'description', 'price','user_id','category_id'];
     public function user(): BelongsTo
     {
@@ -20,18 +20,18 @@ class Ad extends Model
     {
         return $this->belongsTo(Category::class);
     }
-
+    
     public function setAccepted($value)
     {
         $this->is_accepted = $value;
         $this->save();
         return true;
     }
-
-<<<<<<< HEAD
+    
     public static function toBeRevisedCount(){
         return Ad::where('is_accepted', null)->count();
-=======
+        
+    }
     public function toSearchableArray()
     {
         return [
@@ -40,7 +40,7 @@ class Ad extends Model
             'description' =>$this->description,
             'category' =>$this->category
         ];
->>>>>>> c300b1e0ed6d33bc4d562d2bb51b5e896c35da0b
     }
+    
 }
 
