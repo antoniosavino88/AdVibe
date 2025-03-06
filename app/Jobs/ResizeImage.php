8f50e372bb51fdd443bf6dfef12c969c20a5c2a5
Martino <tomaselli.martino@gmail.com>
2025-03-06 12:46:30 +0100

    us 9 completata

--- app/Jobs/ResizeImage.php
+++ app/Jobs/ResizeImage.php
@@ -3,6 +3,7 @@
 namespace App\Jobs;
 
 use Spatie\Image\Image;
+use Spatie\Image\Enums\Unit;
 use Spatie\Image\Enums\CropPosition;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -35,6 +36,14 @@ class ResizeImage implements ShouldQueue
 
         Image::load($srcPath)
         ->crop($w, $h, CropPosition::Center)
+        ->watermark(
+            base_path('resources/img/logo.png'),
+            width:50,
+            height:50,
+            paddingX:5,
+            paddingY:5,
+            paddingUnit: Unit::Percent
+        )
         ->save($destPath);
     }
 }
