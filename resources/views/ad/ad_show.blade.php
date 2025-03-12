727893207c74d204bdd1505b3dbed46ce33c3987
Giacomo Spadafina <giacomospa69@gmail.com>
2025-03-12 16:36:51 +0100

    ultimato stile card; posizionata icona wish-list

--- resources/views/ad/ad_show.blade.php
+++ resources/views/ad/ad_show.blade.php
@@ -1,57 +1,61 @@
 <x-layout>
     @push('title')
-        {{ __('ui.appName') }} - {{ __('ui.detail') }}
+    {{ __('ui.appName') }} - {{ __('ui.detail') }}
     @endpush
     <div class="bg-page-form min-vh-100">
         <div class="container">
             <div class="col-8 col-md-2 position-fixed z-1 my-5">
                 <a href="{{ route('ad_index') }}" class="mb-5 mt-3"><i
-                        class="bi bi-arrow-left-circle fs-1 text-color-5 transition"></i></a>
-            </div>
-            <div class="row height-custom justify-content-between py-5">
-                {{-- COLONNA IMMAGINI --}}
-                <div class="col-12 col-md-5 my-5 py-5">
-                    @if ($ad->images->count() > 0)
+                    class="bi bi-arrow-left-circle fs-1 text-color-5 transition"></i></a>
+                </div>
+                <div class="row height-custom justify-content-evenly py-5">
+                    {{-- COLONNA IMMAGINI --}}
+                    <div class="col-12 col-md-5 my-5 py-5">
+                        @if ($ad->images->count() > 0)
                         <div id="carouselExample" class="carousel slide carousel-dark slide img-show">
                             <div class="carousel-inner">
                                 @foreach ($ad->images as $key => $image)
-                                    <div class="carousel-item @if ($loop->first) active @endif">
-                                        <img src="{{ $image->getUrl(300, 300) }}" class="d-block w-100 rounded shadow"
-                                            alt="Immagine{{ $key + 1 }} dell'articolo {{ $ad->title }}">
-                                    </div>
+                                <div class="carousel-item @if ($loop->first) active @endif">
+                                    <img src="{{ $image->getUrl(300, 300) }}" class="d-block w-100 rounded shadow"
+                                    alt="Immagine{{ $key + 1 }} dell'articolo {{ $ad->title }}">
+                                </div>
                                 @endforeach
                             </div>
                             @if ($ad->images->count() > 1)
-                                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample"
-                                    data-bs-slide="prev">
-                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                    <span class="visually-hidden">Previous</span>
-                                </button>
-                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample"
-                                    data-bs-slide="next">
-                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                    <span class="visually-hidden">Next</span>
-                                </button>
-                            @endif
-                        </div>
-                    @else
-                        <img src="https://picsum.photos/400" alt="Nessuna foto inserita dall'utente">
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample"
+                            data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample"
+                        data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
                     @endif
                 </div>
-                {{-- COLONNA TESTI --}}
-                <div class="col-12 col-md-5 my-5 py-5 text-start">
-                    <div class="row justify-content-center">
-                        <div class="col-12">
-                            <h1 class="text-title text-title-show text-color-5"> {{ $ad->title }}</h1>
-                            <h2 class="text-title">€ {{ $ad->price }}</h2>
-                            <livewire:wish-list :ad="$ad" />
+                @else
+                <img src="https://picsum.photos/400" alt="Nessuna foto inserita dall'utente">
+                @endif
+            </div>
+            {{-- COLONNA TESTI --}}
+            <div class="col-12 col-md-5 my-5 py-5 text-start">
+                <div class="row justify-content-center">
+                    <div class="col-12">
+                        <h1 class="text-title text-title-show text-color-5"> {{ $ad->title }}</h1>
+                        <div class="justify-content-between position-relative row">
+                            <h2 class="text-title col-10">€ {{ $ad->price }}</h2>
+                            <div class="col-2">
+                                <livewire:wish-list :ad="$ad" />
+                            </div>
                         </div>
                     </div>
-                    <div class="row mt-4 p-3 border-custom rounded shadow-sm align-items-center">
-                        <div class="col-7">
-                            <h5 class="mb-2">{{ __('ui.seller') }}: {{ $ad->user->name }}</h5>
-                            <p class="text-muted m-0">{{ __('ui.creationDate') }}:
-                                {{ $ad->created_at->format('d/m/Y') }}</p>
+                </div>
+                <div class="row mt-4 p-3 border-custom rounded shadow-sm align-items-center">
+                    <div class="col-7">
+                        <h5 class="mb-2">{{ __('ui.seller') }}: {{ $ad->user->name }}</h5>
+                        <p class="text-muted m-0">{{ __('ui.creationDate') }}:
+                            {{ $ad->created_at->format('d/m/Y') }}</p>
                         </div>
                         <div class="col-5 d-flex justify-content-end">
                             <a href="#" class="btn btn-sm p-2 btn-custom">{{ __('ui.contactSeller') }}</a>
--- resources/views/livewire/wish-list.blade.php
+++ resources/views/livewire/wish-list.blade.php
@@ -1,10 +1,9 @@
-<div>
-
+<div class="wish-list-icon">
     <button wire:click="toggleSavedAds" class="border-0 bg-transparent">
         @if ($isFavorite)
-            <i class="bi bi-heart-fill text-danger"></i> <!-- Cuore pieno -->
+            <i class="bi bi-heart-fill text-danger fs-5 fw-bold"></i> <!-- Cuore pieno -->
         @else
-            <i class="bi bi-heart"></i> <!-- Cuore vuoto -->
+            <i class="bi bi-heart fs-5 "></i> <!-- Cuore vuoto -->
         @endif
     </button>
 </div>
